c9264920d3953cb025bd56667a2b02a03c4f9b6b
Octave <octave.boelle@gmail.com>
2024-10-27 10:47:00 +0000

    Clion Config

--- install.sh
+++ install.sh
@@ -3,10 +3,7 @@
 apt update
 apt install zip
 
-curl -s "https://get.sdkman.io" | bash
-source "/root/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.2-open
-sdk install kotlin
+apt install make cmake gcc
 
 git config --global user.email "octave.boelle@gmail.com"
 git config --global user.name "Octave"
