e6fa650bb65606550d0dbf3a7f81f56217236ccc
0ctave <octave.boelle@hotmail.fr>
2024-10-29 12:07:22 +0100

    Added DISPLAY

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ git config --global user.name "Octave"
 
 
 echo "=== Display Setup :"
-sudo Xvfb :99 -screen 0 1024x768x16 &
+sudo xpra start :100 --bind-tcp=0.0.0.0:14500 --html=on &
 
 # Export the DISPLAY variable
-export DISPLAY=:99
+export DISPLAY=:100
