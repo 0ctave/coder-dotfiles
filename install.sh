0db227b9bb9bcce411f248a1c577ef897c6768da
0ctave <octave.boelle@hotmail.fr>
2024-10-29 12:14:04 +0100

    Added XPRA

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ git config --global user.name "Octave"
 
 
 echo "=== Display Setup :"
-sudo xpra start :100 --bind-tcp=0.0.0.0:14500 --html=on &
+xpra start :100 --bind-tcp=0.0.0.0:14500 --html=on &
 
 # Export the DISPLAY variable
 export DISPLAY=:100
