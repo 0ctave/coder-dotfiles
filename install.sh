bb984f11eb4ed7ce171e7797fe57bc145f3dbb68
0ctave <octave.boelle@hotmail.fr>
2024-10-29 01:04:06 +0100

    Added sudo

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-apt update
-apt install -y zip
+sudo apt update
+sudo apt install zip
 
-apt install -y make cmake gcc
+curl -s "https://get.sdkman.io" | bash
+sudo source "/root/.sdkman/bin/sdkman-init.sh"
+sdk install java 11.0.2-open
+sdk install kotlin
 
 git config --global user.email "octave.boelle@gmail.com"
 git config --global user.name "Octave"
