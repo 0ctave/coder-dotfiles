cb35565b8d964fbc8d276fd07720343197131ea0
0ctave <octave.boelle@hotmail.fr>
2024-10-30 22:23:52 +0100

    Added XPRA

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@
 sudo apt update
 sudo apt install zip
 
-curl -s "https://get.sdkman.io" | bash
-source "/home/${USER}/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.2-open
-sdk install kotlin
+curl -fsSL https://deno.land/install.sh | sh
+deno install npm:pnpm
 
 git config --global user.email "octave.boelle@gmail.com"
 git config --global user.name "Octave"
