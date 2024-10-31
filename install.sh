9b6ea05d9effb1da244866ccc76b6f64c883b690
0ctave <octave.boelle@hotmail.fr>
2024-10-31 18:40:32 +0100

    Added yarn

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 sudo apt update
 sudo apt install zip
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 nvm install 23
+
+sudo apt update && sudo apt install --no-install-recommends yarn
 
 curl -fsSL https://deno.land/install.sh | sh
 
