53b4b8a29874fde281ce0dca0a83dfa331d5fda8
0ctave <octave.boelle@hotmail.fr>
2024-10-31 14:02:48 +0100

    Remove xpra

--- install.sh
+++ install.sh
@@ -10,35 +10,3 @@ deno install npm:pnpm
 
 git config --global user.email "octave.boelle@gmail.com"
 git config --global user.name "Octave"
-
-
-echo "=== Display Setup :"
-# Set variables
-XPRA_PORT=14500
-
-echo "== Starting Xpra server : ${DISPLAY_NUMBER} ${XPRA_PORT}"
-
-XDG_RUNTIME_DIR=~/.xdg
-XAUTHORITY=~/.Xauthority
-
-mkdir -p $XDG_RUNTIME_DIR
-mkdir -p $XAUTHORITY
-
-# Export the DISPLAY variable
-export DISPLAY=$DISPLAY_NUMBER
-
-# Start XPRA server as the coder user
-xpra start-desktop ${DISPLAY_NUMBER} \
-    --bind-tcp=0.0.0.0:${XPRA_PORT} \
-    --auth=none \
-    --html=on \
-    --tcp-proxy=from \
-    --daemon=no \
-    --exit-with-client=yes \
-    --socket-dirs=~/.xpra \
-    --socket-dir=~/.xpra \
-    --notifications=no \
-    --tcp-auth=none &
-
-
-
