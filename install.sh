94737d50a08f6227c8f40ae7040d8178772fd2ad
0ctave <octave.boelle@hotmail.fr>
2024-10-31 15:18:52 +0100

    Added deno to profile

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt install zip
 
 curl -fsSL https://deno.land/install.sh | sh
 
-export PATH=\"/home/${USER}/.deno/bin:$PATH\"
+export PATH="/home/${USER}/.deno/bin:$PATH"
 deno install npm:pnpm
 
 git config --global user.email "octave.boelle@gmail.com"
