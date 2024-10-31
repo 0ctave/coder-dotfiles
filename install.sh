25dd3231bc0e2967a31c656c16d51f196e887fe7
0ctave <octave.boelle@hotmail.fr>
2024-10-31 18:39:20 +0100

    Added node npm and pnpm

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 sudo apt update
 sudo apt install zip
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+nvm install 23
+
 curl -fsSL https://deno.land/install.sh | sh
 
-export PATH="/home/${USER}/.deno/bin:$PATH"
-deno install npm:pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
 
 git config --global user.email "octave.boelle@gmail.com"
 git config --global user.name "Octave"
